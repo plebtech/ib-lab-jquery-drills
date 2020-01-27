7b234afe88a66cfd12ee7c67862c4bba238fc245
Nick Mc <nbmc@protonmail.com>
2020-01-27 11:40:11 -0600

    finished all lab steps.

--- app.js
+++ app.js
@@ -32,16 +32,34 @@ $("form").submit(function(e) {
 $("body").append('<div></div>');
 
 // append h2 with form text on submit.
-$("form").submit(function() {
-    $('body div').append(`<h2>${$('form :text').val()}</h2>`);
-    listenH2();
-});
+// $("form").submit(function() {
+//     $('body div').append(`<h2>${$('form :text').val()}</h2>`);
+//     listenH2();
+// });
 
 // change background color on mouseover.
 function listenH2() {
     $('h2').mouseover(function() {
     $(this).css("background-color", randomBg());
+    $(this).css("border-radius", randomRadius());
 });
+}
+
+$("body").append('<ul></ul>');
+
+// append li with form text on submit.
+$("form").submit(function() {
+    $('body ul').append(`<li>${$('form :text').val()}</li>`);
+    listenLi();
+});
+
+function listenLi() {
+    $('li').click(function() {
+        $(this).css("background-color", randomBg());
+    });
+    $('li').dblclick(function() {
+        $(this).remove();
+    });
 }
 
 function randomBg() {
@@ -50,4 +68,9 @@ function randomBg() {
     let b = Math.floor(Math.random() * 256);
     let random = "rgb(" + r + "," + g + "," + b + ")";
     return random;
+}
+
+function randomRadius() {
+    let r = Math.floor(Math.random() * 10);
+    return r;
 }
